e3313ea1fc6a8dbc0ca26e451eb7945d24a2ff95
Diefferson Santos <diefferson.sts@gmail.com>
2019-03-18 16:48:03 -0300

    update api

--- lib/api/rest_api.dart
+++ lib/api/rest_api.dart
@@ -4,19 +4,9 @@ import 'package:http/http.dart';
 
 class RestApi{
 
-  static const _BASE_URL = "https://meucartaotransporte.com.br/api/";
+  final String baseUrl;
 
-  Future<Response> getCardBalance(String code, String cpf){
-    return post(_BASE_URL+"v2/cartao",
-        body: {"c": code,"d":cpf, "t": "saldo"},
-        headers: {HttpHeaders.authorizationHeader: "aHR0cHM6Ly9tZXVjYXJ0YW90cmFuc3BvcnRlLmNvbS5ici9hcGkvY2FydGFv"});
-  }
+  RestApi(this.baseUrl);
 
-
-  Future<Response> getExtract(String code, String cpf){
-    return post(_BASE_URL+"v2/cartao",
-        body: {"c": code,"d":cpf, "t": "extrato"},
-        headers: {HttpHeaders.authorizationHeader: "aHR0cHM6Ly9tZXVjYXJ0YW90cmFuc3BvcnRlLmNvbS5ici9hcGkvY2FydGFv"});
-  }
 
 }
